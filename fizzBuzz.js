392176b268b1933e12a08fed5756e30d0026d6d8
Sean <seanthewonderful@gmail.com>
2022-02-10 10:05:08 -0700

    commit

---
+++ fizzBuzz.js
@@ -0,0 +1,27 @@
+// Write your solution below this line:
+for(let i=1; i<51; i++)
+    let output=''
+    if(i%3===0 && i%5===0) output += "fizzbuzz"
+    if(i%3===0) output += "fizz"
+    if(i%5===0) output += "buzz"
+    else output = i {
+        console.log(output)
+    }
+
+// for(let i=1; i<51; i++){
+//     let output=''
+//     if(i%3===0 && i%5===0) output += "fizzbuzz"
+//     if(i%3===0) output += "fizz"
+//     if(i%5===0) output += "buzz"
+//     else output = i
+// }
+
+// for(let i=1; i<51; i++){
+//     if(i%3===0 && i%5===0){
+//         console.log("fizzbuzz")
+//     }if(i%3===0){
+//         console.log("fizz")
+//     }if(i%5===0){
+//         console.log("buzz")
+//     }
+// } 
